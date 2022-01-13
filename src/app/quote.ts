d75727c0aebe043e22564f78a06dd9041b24f02c
Andrewowalla <andrewowalla@gmail.com>
2022-01-13 13:46:30 +0300

    defining the logic

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,10 @@
 export class Quote {
+    
+    upvote:number;
+    downvote:number;
+    constructor(public id: number, public authorName: string, public submittedBy: string, public dateCreated: Date){
+        this.upvote = 0,
+        this.downvote = 0
+    }
+
 }
