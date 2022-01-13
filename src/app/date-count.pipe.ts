c36fb93fcb0d6f7b2f146c37cce9b483eac37944
Andrewowalla <andrewowalla@gmail.com>
2022-01-13 17:25:54 +0300

    pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,12 +1,28 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TmplAstVariable } from '@angular/compiler';
+
 
 @Pipe({
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, ): number {
+
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(todayWithNoTime - value)
+    const secondsInDay = 86400; 
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+    if (dateCounter>=1 && value < todayWithNoTime) {
+      return dateCounter
+    }
+    else{
+      return 0
+    }
+
   }
 
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
 
     public showDetails: boolean;
 
-    constructor(public quote:String, public author: String, public creator: String, public Created: Date){
+    constructor(public quote:String, public Author: String, public Creator: String, public Created: Date){
         this.showDetails=false;
     }
 
